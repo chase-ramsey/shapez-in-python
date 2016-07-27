7fd446e481df27a38c8d5057117e7162400973c0
Chase Ramsey <ramsey.chase@gmail.com>
2016-07-27 11:19:44 -0500

    Created blank classes for three types of shape

--- shapes_test.py
+++ shapes_test.py
@@ -1,4 +1,5 @@
 import unittest
+from sharpshapes import *
 
 class TestCircular(unittest.TestCase):
   pass
---
+++ sharpshapes.py
@@ -0,0 +1,14 @@
+class Circular:
+
+  def __init__(self):
+    pass
+
+class Trilateral:
+
+  def __init__(self):
+    pass
+
+class Quadrilateral:
+
+  def __init__(self):
+    pass
