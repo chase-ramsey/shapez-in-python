4514669aeb3fa5a0b3ced27cd8b38f5d9e32a1d5
Chase Ramsey <ramsey.chase@gmail.com>
2016-07-27 10:58:55 -0500

    Initial files and setup

---
+++ shapes_test.py
@@ -0,0 +1,14 @@
+import unittest
+
+class TestCircular(unittest.TestCase):
+  pass
+
+class TestTrilateral(unittest.TestCase):
+  pass
+
+class TestQuadrilateral(unittest.TestCase):
+  pass
+
+
+if __name__ == '__main__':
+  unittest.main()
